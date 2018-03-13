c65cc71722018b3417631631eee737bd84a8567c
DKostyuk <dkostyuk@ukr.net>
2018-03-13 18:49:09 +0100

    First commit for renew project

--- products/views.py
+++ products/views.py
@@ -19,7 +19,6 @@ def product_line(request, slug):
     product_line = ProductCategory.objects.get(slug=slug)
     products_images = ProductImage.objects.filter(is_active=True, is_main=True, product__is_active=True,
                                                   product__category__is_active=True, product__category__slug=slug)
-    print(products_images)
     session_key = request.session.session_key
     if not session_key:
         request.session.cycle_key()
