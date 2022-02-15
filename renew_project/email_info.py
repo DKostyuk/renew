898aeb6a152e23f59069618baceb9d93765ef415
DKostyuk <dkostyuk@ukr.net>
2022-02-15 08:30:51 +0200

    Update email_info.py

--- renew_project/email_info.py
+++ renew_project/email_info.py
@@ -1,15 +1,15 @@
 # for Zoho mail
 # EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.zoho.eu'
-EMAIL_HOST_USER = 'biuro@renew-polska.pl'
-EMAIL_HOST_PASSWORD = 'njhibyf91'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 # EMAIL_PORT = 587
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-DEFAULT_FROM_EMAIL = "biuro@renew-polska.pl"
+DEFAULT_FROM_EMAIL = ""
 
 EMAIL_PORT = 465
 # EMAIL_BACKEND = 'django_smtp_ssl.SSLEmailBackend'
 EMAIL_USE_SSL = True
 # EMAIL_HOST = 'smtp.zoho.com'
-# EMAIL_HOST_USER = 'dio@streetbarz.com'
-# EMAIL_HOST_PASSWORD = 'password'
+# EMAIL_HOST_USER = ''
+# EMAIL_HOST_PASSWORD = ''
